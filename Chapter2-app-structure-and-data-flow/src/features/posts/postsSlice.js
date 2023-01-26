0e2019eb3f451cd57cd09bc8417b358a54edf9fd
hwangblood <hwangblood@gmail.com>
2023-01-26 14:22:56 +0800

    Chapter 2: Basic posts app

---
+++ Chapter2-app-structure-and-data-flow/src/features/posts/postsSlice.js
@@ -0,0 +1,33 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState = [
+  { id: 1, title: "Learning Redux Toolkit", content: "sdgssgrgerssdfsdgfdsg" },
+  { id: 2, title: "Slices...", content: "sdgssgrgerssdfsdgfdsg" },
+];
+
+const postsSlice = createSlice({
+  name: "posts",
+  initialState,
+  reducers: {
+    postAdded: {
+      reducer(state, action) {
+        state.push(action.payload);
+      },
+      prepare(title, content) {
+        return {
+          payload: {
+            id: nanoid(),
+            title,
+            content,
+          },
+        };
+      },
+    },
+  },
+});
+
+export const selectAllPosts = (state) => state.posts;
+
+export const { postAdded } = postsSlice.actions;
+
+export default postsSlice.reducer;
