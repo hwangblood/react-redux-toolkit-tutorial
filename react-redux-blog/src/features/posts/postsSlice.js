9e206c78aec4527ecababd3c1f77e573445d9104
hwangblood <hwangblood@gmail.com>
2023-02-14 22:56:19 +0800

    Chapter 3: Adding extraReducers and builder cases

--- react-redux-blog/src/features/posts/postsSlice.js
+++ react-redux-blog/src/features/posts/postsSlice.js
@@ -1,4 +1,5 @@
 import { createSlice, nanoid, createAsyncThunk } from "@reduxjs/toolkit";
+import { sub } from "date-fns";
 import axios from "axios";
 
 const POSTS_URL = "http://jsonplaceholder.typicode.com/posts";
@@ -51,6 +52,35 @@ const postsSlice = createSlice({
         existingPost.reactions[reaction]++;
       }
     },
+  },
+  extraReducers(builder) {
+    builder
+      .addCase(fetchPosts.pending, (state, action) => {
+        state.status = "loading";
+      })
+      .addCase(fetchPosts.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        // Adding date and reactions
+        let min = 1;
+        const loadedPosts = action.payload.map((post) => {
+          post.date = sub(new Date(), { minutes: min++ }).toISOString();
+          post.reactions = {
+            thumbsUp: 0,
+            wow: 0,
+            heart: 0,
+            rocket: 0,
+            coffee: 0,
+          };
+          return post;
+        });
+
+        // Add any fetched posts to the array
+        state.posts = state.posts.concat(loadedPosts);
+      })
+      .addCase(fetchPosts.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message;
+      });
   },
 });
 
