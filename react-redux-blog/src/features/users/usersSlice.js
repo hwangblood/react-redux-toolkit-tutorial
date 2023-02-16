6d1c145111689afcf50ac0711927ba84570ca8c6
hwangblood <hwangblood@gmail.com>
2023-02-16 16:27:14 +0800

    Chapter 3: Creating a fetchUsers async thunk

--- react-redux-blog/src/features/users/usersSlice.js
+++ react-redux-blog/src/features/users/usersSlice.js
@@ -1,12 +1,30 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
-const initialState = [
-  { id: "0", name: "DUde Lebowski" },
-  { id: "1", name: "Neil Young" },
-  { id: "2", name: "Dave Grey" },
-];
+import axios from "axios";
 
-const usersSlice = createSlice({ name: "users", initialState, reducers: {} });
+const USERS_URL = "http://jsonplaceholder.typicode.com/users";
+
+const initialState = [];
+
+export const fetchUsers = createAsyncThunk("users/fetchUsers", async () => {
+  try {
+    const response = await axios.get(USERS_URL);
+    return [...response.data];
+  } catch (err) {
+    return Error.message;
+  }
+});
+
+const usersSlice = createSlice({
+  name: "users",
+  initialState,
+  reducers: {},
+  extraReducers(builder) {
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      return action.payload;
+    });
+  },
+});
 
 export const selectAllUsers = (state) => state.users;
 
