71d9d06561ffce53c2259885f8a62bbd7a3180a1
hwangblood <hwangblood@gmail.com>
2023-02-16 16:20:15 +0800

    Chapter 3: PostsExcerpt component && Loading state

--- react-redux-blog/src/features/posts/PostsList.js
+++ react-redux-blog/src/features/posts/PostsList.js
@@ -1,15 +1,12 @@
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { PostsExcerpt } from "./PostsExcerpt";
 import {
   selectAllPosts,
   getPostsStatus,
   getPostsError,
   fetchPosts,
 } from "./postsSlice";
-
-import PostAuthor from "./PostAuthor";
-import ReactionButtons from "./ReactionButtons";
-import TimeAgo from "./TimeAgo";
 
 const PostsList = () => {
   const dispatch = useDispatch();
@@ -24,24 +21,24 @@ const PostsList = () => {
     }
   }, [postsStatus, dispatch]);
 
-  const orderedPosts = posts
-    .slice()
-    .sort((a, b) => b.date.localeCompare(a.date));
-  const renderedPosts = orderedPosts.map((post) => (
-    <article key={post.id}>
-      <h3>{post.title}</h3>
-      <p>{post.content.substring(0, 100)}</p>
-      <p className="postCredit">
-        <PostAuthor userId={post.userId} />
-        <TimeAgo timestamp={post.date} />
-      </p>
-      <ReactionButtons post={post} />
-    </article>
-  ));
+  let content;
+  if (postsStatus === "loading") {
+    content = <p>"Loading"</p>;
+  } else if (postsStatus === "succeeded") {
+    const orderedPosts = posts
+      .slice()
+      .sort((a, b) => b.date.localeCompare(a.date));
+    content = orderedPosts.map((post) => (
+      <PostsExcerpt key={post.id} post={post} />
+    ));
+  } else if (postsStatus === "failed") {
+    content = <p>{error}</p>;
+  }
+
   return (
     <section>
       <h2>Posts</h2>
-      {renderedPosts}
+      {content}
     </section>
   );
 };
