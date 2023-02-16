c55675b7ff4feef0c983ccad96c8239027d7e594
hwangblood <hwangblood@gmail.com>
2023-02-16 16:31:02 +0800

    Chapter 3: Dispatching fetchUsers as App load time

--- react-redux-blog/src/index.js
+++ react-redux-blog/src/index.js
@@ -5,6 +5,10 @@ import { store } from "./app/store";
 import App from "./App";
 import "./index.css";
 
+import { fetchUsers } from "./features/users/usersSlice";
+
+store.dispatch(fetchUsers());
+
 const container = document.getElementById("root");
 const root = createRoot(container);
 
