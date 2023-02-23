466526c9e5428805fb332e66356f1c045203bce4
hwangblood <hwangblood@gmail.com>
2023-02-23 16:28:35 +0800

    Chapter 4: builder case for deletePost

--- react-redux-blog/src/features/posts/postsSlice.js
+++ react-redux-blog/src/features/posts/postsSlice.js
@@ -148,6 +148,17 @@ const postsSlice = createSlice({
         action.payload.date = new Date().toISOString();
         const posts = state.posts.filter((post) => post.id !== id);
         state.posts = [...posts, action.payload];
+      })
+      .addCase(deletePost.fulfilled, (state, action) => {
+        if (!action.payload?.id) {
+          console.log("Update could not complete");
+          console.log(action.payload);
+          return;
+        }
+        const { id } = action.payload;
+        action.payload.date = new Date().toISOString();
+        const posts = state.posts.filter((post) => post.id !== id);
+        state.posts = posts;
       });
   },
 });
