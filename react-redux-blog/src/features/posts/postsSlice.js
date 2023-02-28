363b4e9ceca5d239776299bb900b5be5a1644912
hwangblood <hwangblood@gmail.com>
2023-02-28 16:18:28 +0800

    Chapter 5: entity adapter generated selectors

--- react-redux-blog/src/features/posts/postsSlice.js
+++ react-redux-blog/src/features/posts/postsSlice.js
@@ -148,13 +148,17 @@ const postsSlice = createSlice({
   },
 });
 
-export const selectAllPosts = (state) => state.posts.posts;
+//getSelectors creates these selectors and we rename them with aliases using destructuring
+export const {
+  selectAll: selectAllPosts,
+  selectById: selectPostById,
+  selectIds: selectPostIds,
+  // Pass in a selector that returns the posts slice of state
+} = postsAdapter.getSelectors((state) => state.posts);
+
 export const getPostsStatus = (state) => state.posts.status;
 export const getPostsError = (state) => state.posts.error;
 export const getCount = (state) => state.posts.count;
-
-export const selectPostById = (state, postId) =>
-  state.posts.posts.find((post) => post.id === postId);
 
 export const selectPostByUser = createSelector(
   [selectAllPosts, (state, userId) => userId],
