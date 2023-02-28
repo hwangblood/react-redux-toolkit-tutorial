fe344942a060b2fd1dfbe9c9d9935f2022f87406
hwangblood <hwangblood@gmail.com>
2023-02-28 15:56:22 +0800

    Chapter 5: Normalization

--- react-redux-blog/src/features/posts/PostsExcerpt.js
+++ react-redux-blog/src/features/posts/PostsExcerpt.js
@@ -2,10 +2,9 @@ import PostAuthor from "./PostAuthor";
 import ReactionButtons from "./ReactionButtons";
 import TimeAgo from "./TimeAgo";
 
-import React from "react";
 import { Link } from "react-router-dom";
 
-let PostsExcerpt = ({ post }) => {
+const PostsExcerpt = ({ post }) => {
   return (
     <article>
       <h2>{post.title}</h2>
@@ -19,8 +18,5 @@ let PostsExcerpt = ({ post }) => {
     </article>
   );
 };
-
-// it allows this component to not re-render, if the props that it receives has not changed
-PostsExcerpt = React.memo(PostsExcerpt);
 
 export default PostsExcerpt;
