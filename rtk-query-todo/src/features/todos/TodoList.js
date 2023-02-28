8eb91ab2cbb6fc0c06b5d10fbf5903a2b557ec56
hwangblood <hwangblood@gmail.com>
2023-02-28 19:47:05 +0800

    Chapter 6: Apply useGetTodosQuery in TodoList

--- rtk-query-todo/src/features/todos/TodoList.js
+++ rtk-query-todo/src/features/todos/TodoList.js
@@ -3,8 +3,18 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash, faUpload } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
 
+import { useGetTodosQuery } from "../api/apiSlice";
+
 const TodoList = () => {
   const [newTodo, setNewTodo] = useState("");
+
+  const {
+    data: todos,
+    isLoading,
+    isSuccess,
+    isError,
+    error,
+  } = useGetTodosQuery();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -31,7 +41,13 @@ const TodoList = () => {
   );
 
   let content;
-  // Define conditional content
+  if (isLoading) {
+    content = <p>Loading ...</p>;
+  } else if (isSuccess) {
+    content = JSON.stringify(todos);
+  } else if (isError) {
+    content = <p>{error}</p>;
+  }
 
   return (
     <main>
