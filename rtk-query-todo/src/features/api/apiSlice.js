30cf95966f260bef5d4b32990e7ed984f37ef141
hwangblood <hwangblood@gmail.com>
2023-03-09 16:57:20 +0800

    Chapter 6: Automated Re-fetching

--- rtk-query-todo/src/features/api/apiSlice.js
+++ rtk-query-todo/src/features/api/apiSlice.js
@@ -3,14 +3,16 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3500" }),
+  tagTypes: ["Todos"],
   endpoints: (builder) => ({
-    getTodos: builder.query({ query: () => "/todos" }),
+    getTodos: builder.query({ query: () => "/todos", providesTags: ["Todos"] }),
     addTodo: builder.mutation({
       query: (todo) => ({
         url: "/todos",
         method: "POST",
         body: todo,
       }),
+      invalidatesTags: ["Todos"],
     }),
     updateTodo: builder.mutation({
       query: (todo) => ({
@@ -18,6 +20,7 @@ export const apiSlice = createApi({
         method: "PATCH",
         body: todo,
       }),
+      invalidatesTags: ["Todos"],
     }),
     deleteTodo: builder.mutation({
       query: ({ id }) => ({
@@ -25,6 +28,7 @@ export const apiSlice = createApi({
         method: "DELETE",
         body: id,
       }),
+      invalidatesTags: ["Todos"],
     }),
   }),
 });
